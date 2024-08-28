171724f470aa6ea649fc441ecd480b820183141f
Nicolinho <nicolai.dorka@gmx.de>
2024-08-28 13:18:24 +0200

    rlhf_track_armo_hh10 with dist entropy reward

--- trl/trainer/rloo_trainer.py
+++ trl/trainer/rloo_trainer.py
@@ -390,8 +390,8 @@ class RLOOTrainer(Trainer):
                 kl = logprobs - ref_logprobs
                 non_score_reward = (-args.kl_coef * kl).sum(1)
                 entropy_reward = -args.entropy_coef * reward_dist_entropy.squeeze(1)
-                rlhf_reward = scores_eos + non_score_reward
-                # rlhf_reward = scores_eos + non_score_reward + entropy_reward
+                # rlhf_reward = scores_eos + non_score_reward
+                rlhf_reward = scores_eos + non_score_reward + entropy_reward
 
                 # vectorized RLOO advantages implementation
                 rlhf_reward = rlhf_reward.reshape(args.rloo_k, -1)
